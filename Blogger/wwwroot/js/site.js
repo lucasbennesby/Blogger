1e01b055f4fc2eb2777c353eedc4666733b2d2ee
04540707213 <04540707213@SETIC-20000080>
2025-02-06 13:14:27 -0400

    melhorias na navbar

--- Blogger/wwwroot/js/site.js
+++ Blogger/wwwroot/js/site.js
@@ -4,13 +4,14 @@
     let cadastro = document.querySelector("#cad");
     let formCadastro = document.querySelector("#cardForm");
     let detalhePubli = document.querySelector("#detalhesPubli");
+    let userInfoNavBar = document.querySelector("#userInfo");
 
     if (window.innerWidth <= 768) {
 
         if (login)
             login.classList.add("btn", "btn-outline-secondary", "mb-1");
 
-        if(cadastro)
+        if (cadastro)
             cadastro.classList.add("btn", "btn-outline-secondary");
 
         if (formCadastro) {
@@ -18,21 +19,30 @@
             formCadastro.classList.remove("cardAlt")
         }
 
-        if(detalhePubli)
-            detalhePubli.classList.remove("container")
-            
+        if (detalhePubli)
+            detalhePubli.classList.remove("container")   
+
+        if (userInfoNavBar) 
+            userInfoNavBar.style.textAlign = "center";
+
     } else {
 
-        if(login)
+        if (login)
             login.classList.remove("btn", "btn-outline-secondary");
 
-        if(cadastro)
+        if (cadastro)
             cadastro.classList.remove("btn", "btn-outline-secondary");
 
-        if(formCadastro)
+        if (formCadastro)
             formCadastro.classList.remove("container")
 
-        if(detalhePubli)
+        if (detalhePubli)
             detalhePubli.classList.add("container")
+
+        if (userInfoNavBar) {
+            userInfoNavBar.style.position = "absolute";
+            userInfoNavBar.style.right = "0px";
+            userInfoNavBar.style.marginRight = "3px";
+        }
     }
 });
