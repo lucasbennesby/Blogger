c618fcf38794c4f3c21237e3632e332bb0446240
lucasbennesby <lucasbenesby@gmail.com>
2025-02-14 01:50:02 -0300

    Criando TimeLine

---
+++ Blogger/Controllers/TimeLineController.cs
@@ -0,0 +1,24 @@
+﻿using Blogger.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Blogger.Controllers
+{
+    public class TimeLineController : Controller
+    {
+        private readonly IPublicacaoRepository _publicacaoRepository;
+
+        public TimeLineController(IPublicacaoRepository publicacao)
+        {
+            _publicacaoRepository = publicacao;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var lista = await _publicacaoRepository.Listar();
+            ViewBag.UsuarioLogado = HttpContext.User.Identity?.IsAuthenticated;
+            ViewBag.UsuarioId = HttpContext.User.FindFirstValue("UsuarioId");
+            return View(lista);
+        }
+    }
+}
--- Blogger/Models/Publicacao.cs
+++ Blogger/Models/Publicacao.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Diagnostics.CodeAnalysis;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Blogger.Models
 {
@@ -23,7 +24,7 @@ namespace Blogger.Models
         [StringLength(50)]
         public string NomeAutor { get; set; }
         [MaybeNull]
-        public DateTime DataAtualizacao {  get; set; }
+        public DateTime DataAtualizacao { get; set; }
         public string Imagem { get; set; }
 
         public List<Comentario> Comentarios { get; set; }
@@ -31,8 +32,33 @@ namespace Blogger.Models
         [ForeignKey("UsuarioId")]
 
         public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; }    
+        public Usuario Usuario { get; set; }
         public string Tags { get; set; }
+
+        public string HoraAteAgora(DateTime? data)
+        {
+            DateTime dataAtual = DateTime.Now;
+            TimeSpan diferenca = (TimeSpan)(dataAtual - data);
+
+            var diferencaDeTempoMinutos = (int)diferenca.TotalMinutes;
+            var diferencaDeTempoHoras = (int)diferenca.TotalHours;
+
+            string sResult = diferencaDeTempoMinutos.ToString() + "m";
+
+            if (diferencaDeTempoMinutos > 60)
+            {
+                int result = (int)diferenca.TotalHours;
+                sResult = result.ToString() + "h";
+            }
+            else if(diferencaDeTempoHoras > 24)
+            {
+                int result = (int)diferenca.TotalDays;
+                sResult = result.ToString() + "d";
+            }
+
+            return sResult.ToString();
+                      
+        }
     }
     
 }
